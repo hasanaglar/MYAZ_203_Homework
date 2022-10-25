20dc669c69d9e923ba24bc2bf5c8e2d9a262173f
hasanaglar <201118030@samsun.edu.tr>
2022-10-25 12:48:52 +0300

    hafta _2

---
+++ Hafta_2/Hafta_2/Hafta_2/Employee.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hafta_2
+{
+    public class Employee
+    {
+        private string? _firstName;
+        private string? _lastName;
+        private List<Employee> _list;
+
+        public int Id { get; set; }
+        public string? FirstName 
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+            }
+        }
+        public string? LastName 
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+            }
+        }
+        public int Age { get; set; }
+        public int WorkingYear { get; set; }
+        public decimal Salary { get; set; }
+
+
+
+        public Employee()
+        {
+            _list = new List<Employee>();
+        }
+
+        public Employee(int ıd, string? firstName, string? lastName, int age, int workingYear)
+        {
+            Id = ıd;
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            WorkingYear = workingYear;
+            Salary = 3_000+(workingYear*500);
+            _list = new List<Employee>();
+        }
+
+        public override string ToString()
+        {
+            return $"{Id,-5}" +
+                $"{FirstName,-10}" +
+                $"{LastName,-10}" +
+                $"{Age,5}" +
+                $"{WorkingYear,5}" +
+                $"{Salary,10}";
+        }
+
+        public void Add(Employee employee)
+        {
+            _list.Add(employee);
+        }
+
+        public void Remove(int Id)
+        {
+            _list.RemoveAt(Id);
+        }
+
+        public List<Employee> GetEmployees() => _list;
+    }
+
+}
---
+++ Hafta_2/Hafta_2/Hafta_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using Hafta_2;
+
+namespace Hafta_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employee = new Employee();
+            employee.Add(new Employee(1, "Ahmet", "Yılmaz", 40, 7));
+            employee.Add(new Employee(2, "Merve", "Aslan", 24, 5));
+            employee.Add(new Employee(3, "Elif", "Dağ", 42, 15));
+            employee.Add(new Employee(4, "Hamza", "Hancı", 33, 9));
+            employee.Add(new Employee(5, "Hasan", "Kaan", 28, 5));
+            employee.Add(new Employee(6, "Züheyla", "Göktürk", 27, 3));
+
+            var list = employee.GetEmployees();
+            foreach (var e in list)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
